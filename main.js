4c86e05b06dbbe6bb8234c31957692f36eaf1826
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-03-15 15:54:52 +0100

    album ok

--- main.js
+++ main.js
@@ -3,16 +3,28 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            playlist: [],
+            apiUrl: "", //
+            playlist: [], //array che riempio con API
+            albumDetails: {}, //oggetto vuoto per il singolo album
+            showDetails: false,
             spotify_logo: "https://play-lh.googleusercontent.com/P2VMEenhpIsubG2oWbvuLGrs0GyyzLiDosGTg8bi8htRXg9Uf0eUtHiUjC28p1jgHzo",
         };
     },
     methods: {
+        fetchDisc() { //API singolo album
+            axios.get("http://localhost/php-dischi-json/get-disc.php")
+                .then((response) => {
+                    this.playlist = response.data;
+                });
+        },
 
+        showAlbumDetails(album) {
+            this.showDetails = true;
+            this.albumDetails = album;
+        },
     },
 
     created() {
-        axios.get("http://localhost/php-dischi-json/get-disc.php")
-            .then((response) => { this.playlist = response.data; });
+        this.fetchDisc()
     },
 }).mount('#app');
